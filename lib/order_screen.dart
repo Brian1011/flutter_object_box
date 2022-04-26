6aacc7d1342dde81b3463b57a1cdcde78bca42b6
Brian Mutinda <mutindabrian26@gmail.com>
2022-04-26 09:36:42 +0300

    create order items

--- lib/order_screen.dart
+++ lib/order_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:object_box_project/model/order_model.dart';
 import 'package:object_box_project/objectbox.g.dart';
 
+import 'model/item_model.dart';
+
 class OrderScreen extends StatefulWidget {
   final OrderModel? orderModel;
   final Box<OrderModel> orderBox;
@@ -13,8 +15,25 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
+  List<ItemModel> items = [
+    ItemModel(itemCount: 0, itemName: 'Pizza Salami'),
+    ItemModel(itemCount: 0, itemName: 'Pizza Margaretta'),
+    ItemModel(itemCount: 0, itemName: 'Spaghetti'),
+    ItemModel(itemCount: 0, itemName: 'Lasagne'),
+    ItemModel(itemCount: 0, itemName: 'Rice with Chicken'),
+    ItemModel(itemCount: 0, itemName: 'Fried Noodle'),
+    ItemModel(itemCount: 0, itemName: 'Fried Rice'),
+    ItemModel(itemCount: 0, itemName: 'Salad'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  void saveOrderModel(int index) {
+    widget.orderModel?.items.add(items[index]);
+    widget.orderBox.put(widget.orderModel!);
+    setState(() {});
   }
 }
